a3525dddb674dd6795fac3a773e0d8273e6d1ece
Tigo-cmd <ja16162040@gmail.com>
2024-09-08 10:59:12 +0100

    [implemented prompt for GPT]

--- Tigo.py
+++ Tigo.py
@@ -13,16 +13,30 @@ Copyright (c) 2024 Emmanuel Tigo, All Rights Reserved
 Originally By Nwali Ugonna Emmanuel (Emmanuel Tigo)
 ###################################################################################################################
 """
-
+from typing import Final
 from TigoAi import client
-from telegram.ext import Updater, MessageHandler, Filters
-import telegram
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-openai.api_key = "<YOUR_OPENAI_API_KEY>"
-TELEGRAM_API_TOKEN = "<YOUR_TELEGRAM_BOT_TOKEN>"
 
-messages = [{"role": "system", "content": "You are TelegramGPT, a helpful telegram bot that is always concise and polite in its answers."}]
+BOT_USERNAME: Final = "@TigoGPTBot"
 
+messages = [{"role": "system",
+             "content": "You are TelegramGPT your name is Tigo_bot,"
+                        " a helpful telegram bot that is always concise and polite in its answers."
+                        "you're an AI designed to assist with a wide range of tasks, "
+                        "from answering questions and providing explanations to helping with creative writing, coding,"
+                        " and research. you help with technical problems, brainstorming ideas, "
+                        "and simple information, You're here to assist. Know how you can help today."}]
+
+
+async def Start_command(update: Update, Context: ContextTypes.DEFAULT_TYPE):
+    await Update.message.reply_text("Hello! I'm Tigo_Bot, an AI designed to assist with a wide range of tasks,"
+                                    "from answering questions and providing explanations to helping with creative "
+                                    "writing,"
+                                    "coding, and research. Whether you're looking for help with technical problems, "
+                                    "brainstorming ideas,"
+                                    " or simply need information, I'm here to assist. How can I help you today?")
 
 def text_message(update, context):
     messages.append({"role": "user", "content": update.message.text})
@@ -40,5 +54,3 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), text_message))
 updater.start_polling()
 updater.idle()
-
-
