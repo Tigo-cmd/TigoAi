04891b9a48e54497535e29b1f3759ded8abafa00
Tigo-cmd <ja16162040@gmail.com>
2024-09-08 12:32:33 +0100

    [custom reply]

--- Tigo.py
+++ Tigo.py
@@ -33,7 +33,7 @@ messages = [{"role": "system",
                         " and research. you help with technical problems, brainstorming ideas, "
                         "and simple information, You're here to assist. Know how you can help today."}]
 
-
+# function to handle starting the bot
 async def Start_command(update: Update, Context: ContextTypes.DEFAULT_TYPE):
     await Update.message.reply_text("Hello! I'm Tigo_Bot, an AI designed to assist with a wide range of tasks,"
                                     "from answering questions and providing explanations to helping with creative "
@@ -42,6 +42,18 @@ async def Start_command(update: Update, Context: ContextTypes.DEFAULT_TYPE):
                                     "brainstorming ideas,"
                                     " or simply need information, I'm here to assist. How can I help you today?")
 
+# function to handle the Help response and command
+async def Help_command(update: Update, Context: ContextTypes.DEFAULT_TYPE):
+    await Update.message.reply_text("How can I help you today?")
+
+
+# function to handle the custom reply
+async def Custom_command(update: Update, Context: ContextTypes.DEFAULT_TYPE)
+    await Update.message.reply_text("I'm Here To Help")
+
+
+def response_handler(text: str) -> str:
+    return
 
 def text_message(update, context):
     messages.append({"role": "user", "content": update.message.text})
@@ -52,6 +64,7 @@ def text_message(update, context):
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     update.message.reply_text(text=f"*[Bot]:* {ChatGPT_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
     messages.append({"role": "assistant", "content": ChatGPT_reply})
+
 
 
 updater = Updater(TELEGRAM_API_TOKEN, use_context=True)
