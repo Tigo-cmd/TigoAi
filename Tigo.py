4dc3edb2f1f4b06cf5ea5319f28768619174e4c6
Tigo-cmd <ja16162040@gmail.com>
2024-09-30 13:21:02 +0100

    [Fix]moved interactions to controls

--- Tigo.py
+++ Tigo.py
@@ -1,4 +1,4 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """
 main entry for assistant
 """
@@ -17,23 +17,43 @@ Originally By Nwali Ugonna Emmanuel (Emmanuel Tigo)
 ###################################################################################################################
 """
 
-from models.source import TigoGroq, load_dotenv, find_patterns
+from models.source import TigoGroq, load_dotenv
 from argparse import ArgumentParser, Namespace
+from TigoAi.models.controls import pass_gen
+# initialize color for crossPlatform compatibility
+
 
 load_dotenv()  # typically loads API keys form underlining env file
 client = TigoGroq()  # calls groqAPI client
 
-# entry = ArgumentParser()
-#
-# entry.add_argument("")
 
-message = input(">")
-pattern = "create"
+def main() -> None:
+
+    # creating an into texts with argparse module
+    entry = ArgumentParser(description="Tigo CLI Assistant", prog="Tigo")
+    entry.add_argument('-i', '--interactive', type=str, nargs="*", choices=["chat", "start"], help="Runs in "
+                                                                                                   "interactive mode")
+    entry.add_argument('-p', '--passgen', type=int, nargs='?', metavar="password length", help="password generator")
+    args: Namespace = entry.parse_args()
+
+    length = args.passgen
+# arguments implementations
+
+    # interactive mode with Tigo
+    if args.interactive:
+        for _ in args.interactive:
+            if len(args.interactive) == 0:
+                print(args.interactive)
+            interactive_mode()
+
+    # password generator
+    if args.passgen:
+        i = args.passgen
+        print(pass_gen(i if i is not None else 16))
 
 
-@find_patterns
-def printOut():
-    print(message, pattern)
-
-
-printOut(message, pattern)
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
