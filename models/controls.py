a67c388d01e461892765fb0ee10937bde519442b
Tigo-cmd <ja16162040@gmail.com>
2024-12-28 20:54:35 +0100

    handled and removed some redunant importations

--- models/controls.py
+++ models/controls.py
@@ -8,10 +8,6 @@ from typing import Callable
 from colorama import Fore, Style, init
 from string import ascii_letters, digits, punctuation
 from secrets import choice
-from TigoAi.models.source import TigoGroq
-
-
-client = TigoGroq()
 
 
 def clear_screen() -> None:
@@ -33,7 +29,6 @@ def pass_gen(length: int = 16) -> str:
     :param length: length of the password.
     :return:  password generated
     """
-    default = 12
     passGen: Callable[[int], str] = lambda x: ''.join(choice(ascii_letters + digits + punctuation) for _ in range(x))
 
     return passGen(length)
