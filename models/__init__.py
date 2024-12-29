695d616c6364a831b576cfa22b441fe93702cb31
Tigo-cmd <ja16162040@gmail.com>
2024-12-29 12:41:25 +0100

    nothing to do here

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,1 @@
-from TigoAi.models.controls import pass_gen, clear_screen
 
-
-keywords: list[str] = [
-    'create a file to do something',
-    'analyze a giving error',
-    'debug a file with some error to be provided',
-    'compile or run a file and print output',
-]
