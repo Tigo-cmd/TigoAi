9dfaf6612477adfbfb0225e517f6ba823074b982
Tigo-cmd <ja16162040@gmail.com>
2024-12-29 12:35:29 +0100

    added regex function to detect filenames

--- models/controls.py
+++ models/controls.py
@@ -8,6 +8,7 @@ from typing import Callable
 from colorama import Fore, Style, init
 from string import ascii_letters, digits, punctuation
 from secrets import choice
+import re
 
 
 def clear_screen() -> None:
@@ -19,6 +20,16 @@ def clear_screen() -> None:
         os.system("cls")
     else:
         os.system('clear -x')
+
+
+def extract_filenames(text):
+    # Define the updated regex pattern to match more complex filenames
+    pattern = r'\b[\w\.-]+(?:\.[a-zA-Z0-9]+)\b'
+
+    # Use re.findall() to find all occurrences of filenames in the text
+    filenames = re.findall(pattern, text)
+
+    return filenames
 
 
 def pass_gen(length: int = 16) -> str:
